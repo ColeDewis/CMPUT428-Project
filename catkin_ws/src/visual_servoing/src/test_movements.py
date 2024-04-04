f6fb375a977fe29ff4387c02583bb09a17369131
ColeDewis <coledewis@gmail.com>
2024-04-04 22:54:16 +0000

    distance math stuff and some tweaks

--- catkin_ws/src/visual_servoing/src/test_movements.py
+++ catkin_ws/src/visual_servoing/src/test_movements.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     print("BEFORE: ", rospy.get_rostime())
     success = gen3.send_joint_angles(angles)
     print("AFTER: ", rospy.get_rostime())
-    gen3.send_gripper_command(0.5)
+    gen3.send_gripper_command(0.99)
     print("Done. Joints:", gen3.position)
